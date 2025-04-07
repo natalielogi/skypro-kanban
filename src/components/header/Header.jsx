fef877abc04fe0000099a5693d233565a40537a8
natalielogi <logilogi93@yandex.ru>
2025-04-07 15:32:04 +0300

    исправила модальное окно

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import PopUser from "../popups/PopUser/PopUser";
 
 const Header = () => {
+  const [isUserVisible, setIsUserVisible] = useState(false);
+  const modalref = useRef(null);
+
+  const toggleUserModal = (e) => {
+    e.preventDefault();
+    setIsUserVisible((prev) => !prev);
+  };
+
+  const closeModal = () => {
+    setIsUserVisible(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        isUserVisible &&
+        modalref.current &&
+        !modalref.current.contains(event.target)
+      ) {
+        setIsUserVisible(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isUserVisible]);
+
   return (
     <header className="header">
       <div className="container">
@@ -19,9 +48,16 @@ const Header = () => {
             <button className="header__btn-main-new _hover01" id="btnMainNew">
               <a href="#popNewCard">Создать новую задачу</a>
             </button>
-            <a href="#user-set-target" className="header__user _hover02">
+            <a
+              href="#user-set-target"
+              className="header__user _hover02"
+              onClick={toggleUserModal}
+            >
               Ivan Ivanov
             </a>
+            {isUserVisible && (
+            <PopUser ref={modalref} onClose={closeModal} />
+            )}
           </nav>
         </div>
       </div>
--- src/components/popups/PopUser/PopUser.jsx
+++ src/components/popups/PopUser/PopUser.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { forwardRef } from "react";
 
-const PopUser = () => {
+const PopUser = forwardRef(({ onClose }, ref) => {
   return (
-    <div className="header__pop-user-set pop-user-set" id="user-set-target">
-      <a href="">x</a>
+    <div
+      className="header__pop-user-set pop-user-set"
+      id="user-set-target"
+      ref={ref}
+    >
+      <a
+        href=""
+        onClick={(e) => {
+          e.preventDefault();
+          onClose();
+        }}
+      >
+        x
+      </a>
       <p className="pop-user-set__name">Ivan Ivanov</p>
       <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
       <div className="pop-user-set__theme">
         <p>Темная тема</p>
         <input type="checkbox" className="checkbox" name="checkbox" />
       </div>
-      <button type="button" className="_hover03">
+      <button type="button" className="_hover03" onClick={onClose}>
         <a href="#popExit">Выйти</a>
       </button>
     </div>
   );
-};
+});
 
 export default PopUser;
