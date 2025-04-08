ee5e26e47693a188320173953c1c4ea18969a6bc
natalielogi <logilogi93@yandex.ru>
2025-04-08 22:52:06 +0300

    main styled

---
+++ src/components/main/main.styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { Container as BaseContainer } from "../header/Header.styled";
+
+export const MainWrapper = styled.main`
+  width: 100%;
+  background-color: #eaeef6;
+`;
+
+export const MainContainer = styled(BaseContainer)``;
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-size: 24px;
+  color: #333;
+`;
+
+export const MainBlock = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 25px 0 49px;
+`;
+
+export const MainContent = styled.div`
+  width: 100%;
+  display: flex;
+`;
