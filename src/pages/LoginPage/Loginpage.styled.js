765121752f5175d3d2c3b8dde3ef21eef6607fa5
natalielogi <logilogi93@yandex.ru>
2025-04-13 17:53:23 +0300

    первая попытка регистрации и входа

---
+++ src/pages/LoginPage/Loginpage.styled.js
@@ -0,0 +1,132 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  background-color: #eaeef6;
+`;
+
+const ContainerSignin = styled.div`
+  display: block;
+  width: 100vw;
+  min-height: 100vh;
+  margin: 0 auto;
+`;
+
+const Modal = styled.div`
+  width: 100%;
+  height: 100%;
+  min-width: 320px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalBlock = styled.div`
+  display: block;
+  margin: 0 auto;
+  background-color: #ffffff;
+  max-width: 368px;
+  width: 100%;
+  padding: 50px 60px;
+  border-radius: 10px;
+  border: 0.7px solid #d4dbe5;
+  box-shadow: 0px 4px 67px -12px rgba(0, 0, 0, 0.13);
+
+  @media screen and (max-width: 375px) {
+    padding: 0 16px;
+    border-radius: none;
+    border: none;
+    box-shadow: none;
+  }
+`;
+
+const ModalTitle = styled.div`
+  h2 {
+    text-align: center;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 30px;
+    letter-spacing: -0.6px;
+    margin-bottom: 20px;
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  min-width: 100%;
+  border-radius: 8px;
+  border: 0.7px solid rgba(148, 166, 190, 0.4);
+  outline: none;
+  padding: 10px 8px;
+  margin-bottom: 7px;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+
+  &::placeholder {
+    color: #94a6be;
+  }
+`;
+
+const ButtonEnter = styled.button`
+  width: 100%;
+  height: 30px;
+  background-color: #565eef;
+  border-radius: 4px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: 500;
+  letter-spacing: -0.14px;
+  color: #ffffff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #33399b;
+  }
+
+  @media screen and (max-width: 375px) {
+    height: 40px;
+  }
+`;
+
+const FormGroup = styled.div`
+  text-align: center;
+
+  p,
+  a {
+    color: rgba(148, 166, 190, 0.4);
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 150%;
+    letter-spacing: -0.14px;
+  }
+
+  a {
+    text-decoration: underline;
+  }
+`;
+
+const Link = styled.a`
+  color: rgba(148, 166, 190, 0.4);
+  cursor: pointer;
+`;
---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as S from "./RegisterPage.styled"
+
+const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Регистрация:", formData);
+    navigate("/login");
+  };
+
+  return (
+    <S.Wrapper>
+      <S.ContainerSignup>
+        <S.Modal>
+          <S.ModalBlock>
+            <S.ModalTitle>
+              <h2>Регистрация</h2>
+            </S.ModalTitle>
+            <S.Form onSubmit={handleSubmit}>
+              <S.Input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Имя"
+                required
+              />
+              <S.Input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Эл. почта"
+                required
+              />
+              <S.Input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Пароль"
+                required
+              />
+              <S.ButtonSignUp type="submit">Зарегистрироваться</S.ButtonSignUp>
+              <S.FormGroup>
+                <p>
+                  Уже есть аккаунт? <Link href="/login">Войдите здесь</Link>
+                </p>
+              </S.FormGroup>
+            </S.Form>
+          </S.ModalBlock>
+        </S.Modal>
+      </S.ContainerSignup>
+    </S.Wrapper>
+  );
+};
+
+export default RegisterPage;
