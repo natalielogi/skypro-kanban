31419accdda15572fe49e2e8be5d9a320ecebce9
natalielogi <logilogi93@yandex.ru>
2025-04-13 19:46:56 +0300

    обновила cardpage и addtaskpage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,16 +2,10 @@ import { Outlet } from "react-router-dom";
 import React from "react";
 import Header from "../components/header/Header";
 import Main from "../components/main/main";
-import PopBrowse from "../components/popups/PopBrowse/PopBrowse";
-import PopNewCard from "../components/popups/PopNewCard/PopNewCard";
 
 function MainPage() {
   return (
     <div className="wrapper">
-      {/* Модальные окна */}
-      <PopBrowse />
-      <PopNewCard />
-      {/* Основной контент */}
       <Header />
       <Main />
       <Outlet />
